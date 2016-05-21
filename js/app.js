4be717d4b2f916104d3053b76af4b899f8346f89
Makhmutov Damir <iam@damir.space>
2016-05-21 20:53:18 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -14,21 +14,20 @@ function onFileSelected(e) {
 
 function parse(file) {
     var results = Object.create(null);
+    var i = 0;
 
     Papa.parse(file, {
-        header: true,
         step: function (result, parser) {
             var row = result.data.pop();
+            var criteria = row[1];
+            var param1 = row[2];
+            var param2 = row[3];
+            var val = Number(row[4]);
+            var key = [criteria, param1, param2].filter(identity).join(':');
 
-            if (!row.Date) {
+            if (i++ == 0 || !key) {
                 return false;
             }
-
-            var criteria = row.Criterion;
-            var param1 = row['Parameter 1'];
-            var param2 = row['Parameter 2'];
-            var val = Number(row.Value);
-            var key = [criteria, param1, param2].filter(identity).join(':');
 
             if (typeof results[key] === 'undefined') {
                 results[key] = 0;
